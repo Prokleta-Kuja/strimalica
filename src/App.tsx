b5c97212d8298da444ee779caff81fb989f21cee
Prokleta-Kuja <Prokleta.Kuja@gmail.com>
2022-12-15 13:36:11 +0000

    Refresh, icons

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,46 @@ function App() {
   const radio = useRadio();
   const classes = `list-group-item list-group-item-action list-group-item-${
     radio.isPlaying ? "danger" : "primary"
-  } text-center`;
+    } text-center`;
+  const close = () => {
+    window.opener = null;
+    window.open('', '_self');
+    window.close();
+  };
+  const refresh = () => {
+    window.location.reload();
+  };
 
   return (
-    <div className="container-fluid">
+    <div className="container-fluid mt-2">
       <div className="list-group list-group-flush">
         {Config.Streams.map((r) => (
           <Radio key={r.id} {...r} />
         ))}
         {radio.currentStation && (
           <button type="button" onClick={radio.toggle} className={classes}>
-            {radio.isPlaying ? "⏸" : "⏵"}
+            {radio.isPlaying ? <i className="bi bi-stop-circle-fill"></i> : <i className='bi bi-play-circle-fill'></i>}
           </button>
         )}
       </div>
-      <div className="text-center mt-4">
-        <div className="btn-group">
-          {Config.VolumeSteps.map((v) => (
-            <Volume key={v} vol={v} />
-          ))}
+      <div className="d-flex justify-content-between">
+        <div className="mt-4">
+          <button className="btn btn-primary" onClick={refresh}>
+            <i className="bi bi-arrow-clockwise"></i>
+          </button>
+        </div>
+        <div className="text-center mt-4">
+          <div className="btn-group">
+            {Config.VolumeSteps.map((v) => (
+              <Volume key={v} vol={v} />
+              ))}
+          </div>
+        </div>
+        <div className="mt-4">
+          {/* about:config -> allow_scripts_to_close_windows : true */}
+          <button className="btn btn-danger" onClick={close}>
+            <i className="bi bi-x-circle-fill"></i>
+          </button>
         </div>
       </div>
     </div>
